0b6b08218f010e448e621113fff127c423f59527
miveradev <154668151+miveradev@users.noreply.github.com>
2024-08-31 13:18:30 +0200

    feat: added supabase data

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly SUPABASE_URL: "https://trzdgjmwbfpfficnkglo.supabase.co"
+  readonly PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyemRnam13YmZwZmZpY25rZ2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwOTk2OTEsImV4cCI6MjA0MDY3NTY5MX0.RFM8Mut8Fu-kF5ycFA1jxN8nbgaCp72PhcEx1I4wFwo"
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
